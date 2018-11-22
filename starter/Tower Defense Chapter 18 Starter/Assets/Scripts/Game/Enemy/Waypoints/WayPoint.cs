2b6544d6eff954a5e7e346ac9fcb60f427fed73b
Robert <Robertwilliamworld@hotmail.com>
2018-11-22 09:55:00 -0500

    Added the path scripts to the lab

---
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Enemy/Waypoints/WayPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+public class WayPoint : MonoBehaviour
+{
+
+    public static WayPoint Instance;
+
+    public List<Path> Paths = new List<Path>();
+    void Awake()
+    {
+  
+        Instance = this;
+    }
+
+    public Vector3 GetSpawnPosition(int pathIndex)
+    {
+        return Paths[pathIndex].WayPoints[0].position;
+    }
+}
+
+[System.Serializable]
+public class Path
+{
+    public List<Transform> WayPoints = new List<Transform>();
+}
